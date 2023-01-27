584363d62f1576aa1344c87275556ca782aad858
Olivier BATTINI <olivier@olivierbattini.fr>
2023-01-27 15:13:55 +0100

    feat: basic api with index and system stats
    Add basic API index and stats with CPU and RAM %
    
    Update docs to current dev state

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import psutil
 from flask import Flask, jsonify
 
 api = Flask(__name__)
@@ -5,8 +6,23 @@ api = Flask(__name__)
 
 @api.route('/')
 def index():
-    return jsonify({'message': 'Hello, World!'})
+    return jsonify({
+        'message': 'Hello, World!',
+    })
+
+
+@api.route('/stats')
+def stats():
+    return jsonify({
+        'cpu_count': psutil.cpu_count(),
+        'cpu_load_percent': psutil.cpu_percent(),
+        'ram_load_percent': psutil.virtual_memory().percent,
+    })
 
 
 if __name__ == '__main__':
+    print("Starting app...")
     api.run()
+    print("Stopping app...")
+
+print("Exiting...")
