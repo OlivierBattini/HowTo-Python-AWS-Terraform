28517e59e8ce3d579a612f06bee598dcbf10f467
Olivier BATTINI <olivier@olivierbattini.fr>
2023-01-27 16:18:26 +0100

    refactor: modify amazon image to test terraform apply
    Modify Amazon Image to ami-0afd55c0c8a52973a (Ubutu image) to test Terraform changes application

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0ca5ef73451e16dc1"
+  ami           = "ami-0afd55c0c8a52973a"
   instance_type = "t2.micro"
 
   tags = {
